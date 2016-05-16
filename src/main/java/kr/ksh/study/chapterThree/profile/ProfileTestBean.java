f2af88df521f7e6a43920b9c32de402203d38078
KimSanghyun <ksh01273@kesti.co.kr>
2016-05-16 21:08:57 +0900

    SpringinAction Chapter3

--- src/main/java/kr/ksh/study/chapterThree/profile/ProfileTestBean.java
+++ src/main/java/kr/ksh/study/chapterThree/profile/ProfileTestBean.java
@@ -2,9 +2,10 @@ package kr.ksh.study.chapterThree.profile;
 
 public class ProfileTestBean {	
 	private String rootPath; 
+	
 	public String getRootPath() {
 		return rootPath;
-	} 
+	}
 	public void setRootPath(String rootPath) {
 		this.rootPath = rootPath;
 	}
